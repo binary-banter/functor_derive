ba0bbd3928b5e1d266e2951824cdf636a776e654
jonathan <jonathantbrouwer@gmail.com>
2023-12-10 22:36:51 +0100

    Names working

--- functor_derive/tests/attributes.rs
+++ functor_derive/tests/attributes.rs
@@ -45,27 +45,59 @@ fn default_multiple_out_of_order() {
     );
 }
 
-// #[test]
-// fn map_specified_and_name() {
-//     #[derive(Functor)]
-//     #[functor(S, S as stuff)]
-//     struct MyType<S> {
-//         v1: S,
-//         v2: bool,
-//     }
-//
-//     let x = MyType {
-//         v1: 42usize,
-//         v2: true,
-//     };
-//
-//     assert_eq!(
-//         x.fmap(|x| x as u64).type_id(),
-//         TypeId::of::<MyType<u64>>()
-//     );
-//
-//     assert_eq!(
-//         x.fmap_stuff(|x| x as u64).type_id(),
-//         TypeId::of::<MyType<u64>>()
-//     );
-// }
+#[test]
+fn map_specified_and_name() {
+    #[derive(Copy, Clone, Functor)]
+    #[functor(S, S as stuff, S as gunk)]
+    struct MyType<S> {
+        v1: S,
+        v2: bool,
+    }
+
+    let x = MyType {
+        v1: 42usize,
+        v2: true,
+    };
+
+    assert_eq!(
+        x.fmap(|x| x as u64).type_id(),
+        TypeId::of::<MyType<u64>>()
+    );
+
+    assert_eq!(
+        x.fmap_stuff(|x| x as u64).type_id(),
+        TypeId::of::<MyType<u64>>()
+    );
+
+    assert_eq!(
+        x.fmap_gunk(|x| x as u64).type_id(),
+        TypeId::of::<MyType<u64>>()
+    );
+}
+
+#[test]
+fn map_multi() {
+    #[derive(Copy, Clone, Functor)]
+    #[functor(T, S as wonky)]
+    struct MyType<S, T> {
+        v1: S,
+        v2: bool,
+        v3: T
+    }
+
+    let x = MyType {
+        v1: 42usize,
+        v2: true,
+        v3: false,
+    };
+
+    assert_eq!(
+        x.fmap(|x| x as u64).type_id(),
+        TypeId::of::<MyType<usize, u64>>()
+    );
+
+    assert_eq!(
+        x.fmap_wonky(|x| x as u64).type_id(),
+        TypeId::of::<MyType<u64, bool>>()
+    );
+}
--- functor_derive_lib/src/lib.rs
+++ functor_derive_lib/src/lib.rs
@@ -53,6 +53,15 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             &source_args,
         ));
     }
+    for (param, name) in attribute.name_map {
+        tokens.extend(generate_named_impl(
+            &param,
+            &name,
+            &def_name,
+            &source_params,
+            &source_args,
+        ));
+    }
     tokens.extend(generate_refs_impl(
         &input.data,
         &def_name,
@@ -123,12 +132,12 @@ fn generate_refs_impl(
 }
 
 fn generate_default_impl(
-    default: &Ident,
+    param: &Ident,
     def_name: &Ident,
     source_params: &Vec<GenericParam>,
     source_args: &Vec<GenericArgument>,
 ) -> TokenStream {
-    let (default_idx_total, default_idx_types) = find_index(source_params, default);
+    let (default_idx_total, default_idx_types) = find_index(source_params, param);
 
     // Create generic arguments for the target. We use `__B` for the mapped generic.
     let mut target_args = source_args.clone();
@@ -141,15 +150,15 @@ fn generate_default_impl(
     let default_try_map = format_ident!("__try_fmap_{default_idx_types}_ref");
 
     quote!(
-        impl<#(#source_params),*> ::functor_derive::Functor<#default> for #def_name<#(#source_args),*> {
+        impl<#(#source_params),*> ::functor_derive::Functor<#param> for #def_name<#(#source_args),*> {
             type Target<__B> = #def_name<#(#target_args),*>;
 
-            fn fmap<__B>(self, __f: impl Fn(#default) -> __B) -> #def_name<#(#target_args),*> {
+            fn fmap<__B>(self, __f: impl Fn(#param) -> __B) -> #def_name<#(#target_args),*> {
                 use ::functor_derive::*;
                 self.#default_map(&__f)
             }
 
-            fn try_fmap<__B, __E>(self, __f: impl Fn(#default) -> Result<__B, __E>) -> Result<#def_name<#(#target_args),*>, __E> {
+            fn try_fmap<__B, __E>(self, __f: impl Fn(#param) -> Result<__B, __E>) -> Result<#def_name<#(#target_args),*>, __E> {
                 use ::functor_derive::*;
                 self.#default_try_map(&__f)
             }
@@ -157,100 +166,39 @@ fn generate_default_impl(
     )
 }
 
-// fn generate_impl(input: &DeriveInput, def_name: &Ident, functor_param: Ident, name_suffix: Option<Ident>) -> TokenStream {
-// // Get the generic parameters *including* bounds and other attributes.
-//     let gen_params = input.generics.params.iter().cloned().collect::<Vec<_>>();
-//
-//     // Find the position and type of the generic parameter. Aborts if absent.
-//     let (functor_param_idx, functor_param_type) = gen_params
-//         .iter()
-//         .enumerate()
-//         .find_map(|(idx, param)| match param {
-//             GenericParam::Type(typ) if typ.ident == functor_param => Some((idx, typ)),
-//             _ => None,
-//         })
-//         .unwrap_or_else(|| {
-//             abort_call_site!(
-//                 "The generic parameter `{}` could not be found in the definition of `{}`.",
-//                 functor_param,
-//                 def_name
-//             )
-//         });
-//
-//     // Maps the generic parameters to generic arguments for the source.
-//     let source_args = gen_params
-//         .iter()
-//         .map(|param| match param {
-//             GenericParam::Lifetime(l) => GenericArgument::Lifetime(l.lifetime.clone()),
-//             GenericParam::Type(t) => GenericArgument::Type(Type::Path(TypePath {
-//                 qself: None,
-//                 path: Path::from(PathSegment::from(t.ident.clone())),
-//             })),
-//             GenericParam::Const(c) => GenericArgument::Const(Expr::Path(ExprPath {
-//                 attrs: vec![],
-//                 qself: None,
-//                 path: Path::from(PathSegment::from(c.ident.clone())),
-//             })),
-//         })
-//         .collect::<Vec<_>>();
-//
-//     // Create generic arguments for the target. We use `__B` for the mapped generic.
-//     let mut target_args = source_args.clone();
-//     target_args[functor_param_idx] = GenericArgument::Type(Type::Path(TypePath {
-//         qself: None,
-//         path: Path::from(PathSegment::from(format_ident!("__B"))),
-//     }));
-//
-//     todo!()
-//     // Generate body of the `fmap` implementation.
-//     let fmap_body =
-//         generate_fmap_body::generate_fmap_body(&input.data, &def_name, &functor_param, false);
-//     let try_fmap_body =
-//         generate_fmap_body::generate_fmap_body(&input.data, &def_name, &functor_param, true);
-//     //
-//     // // If there are no bounds on the generics, generate tokens for `Functor` trait impl for the given definition.
-//     // // Otherwise, generate `fmap` impl for the given definition.
-//     // if functor_param_type.bounds.is_empty() && name_suffix.is_none() {
-//     //     quote!(
-//     //         impl<#(#gen_params),*> ::functor_derive::Functor<#functor_param> for #def_name<#(#source_args),*> {
-//     //             type Target<__B> = #def_name<#(#target_args),*>;
-//     //
-//     //             fn fmap_ref<__B>(self, __f: &impl Fn(#functor_param) -> __B) -> #def_name<#(#target_args),*> {
-//     //                 #fmap_body
-//     //             }
-//     //
-//     //             fn try_fmap_ref<__B, __E>(self, __f: &impl Fn(#functor_param) -> Result<__B, __E>) -> Result<#def_name<#(#target_args),*>, __E> {
-//     //                 Ok(#try_fmap_body)
-//     //             }
-//     //         }
-//     //     )
-//     // } else {
-//     //     let bounds = &functor_param_type.bounds;
-//     //
-//     //     let suffix = name_suffix.map(|name_suffix| format!("_{name_suffix}")).unwrap_or_default();
-//     //     let fmap = format_ident!("fmap{suffix}");
-//     //     let fmap_ref = format_ident!("fmap_ref{suffix}");
-//     //     let try_fmap = format_ident!("try_fmap{suffix}");
-//     //     let try_fmap_ref = format_ident!("try_fmap_ref{suffix}");
-//     //
-//     //     quote!(
-//     //         impl<#(#gen_params),*> #def_name<#(#source_args),*> {
-//     //             pub fn #fmap<__B: #bounds>(self, __f: impl Fn(#functor_param) -> __B) -> #def_name<#(#target_args),*> {
-//     //                 self.fmap_ref(&__f)
-//     //             }
-//     //
-//     //             pub fn #fmap_ref<__B: #bounds>(self, __f: &impl Fn(#functor_param) -> __B) -> #def_name<#(#target_args),*> {
-//     //                 #fmap_body
-//     //             }
-//     //
-//     //             pub fn #try_fmap<__B: #bounds, __E>(self, __f: impl Fn(#functor_param) -> Result<__B, __E>) -> Result<#def_name<#(#target_args),*>, __E> {
-//     //                 self.try_fmap_ref(&__f)
-//     //             }
-//     //
-//     //             pub fn #try_fmap_ref<__B: #bounds, __E>(self, __f: &impl Fn(#functor_param) -> Result<__B, __E>) -> Result<#def_name<#(#target_args),*>, __E> {
-//     //                 Ok(#try_fmap_body)
-//     //             }
-//     //         }
-//     //     )
-//     // }.into()
-// }
+fn generate_named_impl(
+    param: &Ident,
+    name: &Ident,
+    def_name: &Ident,
+    source_params: &Vec<GenericParam>,
+    source_args: &Vec<GenericArgument>,
+) -> TokenStream {
+    let (default_idx_total, default_idx_types) = find_index(source_params, param);
+
+    // Create generic arguments for the target. We use `__B` for the mapped generic.
+    let mut target_args = source_args.clone();
+    target_args[default_idx_total] = GenericArgument::Type(Type::Path(TypePath {
+        qself: None,
+        path: Path::from(PathSegment::from(format_ident!("__B"))),
+    }));
+
+    let fmap_name = format_ident!("fmap_{name}");
+    let try_fmap_name = format_ident!("try_fmap_{name}");
+
+    let fmap = format_ident!("__fmap_{default_idx_types}_ref");
+    let fmap_try = format_ident!("__try_fmap_{default_idx_types}_ref");
+
+    quote!(
+        impl<#(#source_params),*> #def_name<#(#source_args),*> {
+            fn #fmap_name<__B>(self, __f: impl Fn(#param) -> __B) -> #def_name<#(#target_args),*> {
+                use ::functor_derive::*;
+                self.#fmap(&__f)
+            }
+
+            fn #try_fmap_name<__B, __E>(self, __f: impl Fn(#param) -> Result<__B, __E>) -> Result<#def_name<#(#target_args),*>, __E> {
+                use ::functor_derive::*;
+                self.#fmap_try(&__f)
+            }
+        }
+    )
+}
